db7e16662e05fad313c7bcf539a3f7b5a39aec90
Syed Mohsin <syedm.90@gmail.com>
2017-10-20 20:54:51 -0400

    add helper function to generate random events for a world

--- public/main.js
+++ public/main.js
@@ -1,8 +1,68 @@
+var ViaGogoWorld = {};
+
+ViaGogoWorld.generateWorld = function(numEvents, xDim, yDim) {
+  var world = {
+    xDim: xDim,
+    yDim: yDim,
+    events: [],
+  };
+
+  var worldWidth = (2 * xDim + 1),
+      worldHeight = (2 * yDim + 1),
+      maxEvents =  worldWidth * worldHeight,
+      eventCount = 0,
+      eventId = 1,
+      seenCoords = new Set();
+
+  console.log(maxEvents, worldWidth, worldHeight);
+
+  // handle case where numEvents is too large
+  if (numEvents > maxEvents) {
+    numEvents = Math.floor(maxEvents / 2);
+  }
+
+  var randXPos, randYPos, seenEvent, event;
+
+  while (eventCount < numEvents) {
+    randXPos = Math.floor(Math.random() * worldWidth - xDim);
+    randYPos = Math.floor(Math.random() * worldHeight - yDim);
+
+    seenCoord = JSON.stringify(randXPos + ',' + randYPos);
+
+    // do not add another event for seen coordinate pair
+    if (seenCoords.has(seenCoord)) {
+      continue;
+    }
+
+    event = {
+      id: eventId++, // increment id
+      xPos: randXPos,
+      yPos: randYPos,
+    };
+
+    world.events.push(event);
+
+    seenCoords.add(seenCoord);
+    eventCount++;
+  }
+
+  return world;
+};
+
 (function() {
+  var buttonSelector = '[name=find_tickets]';
+  var inputSelector = '[name=coordinates]';
+
+  var numEvents = 99999999;
+  var worldXDimension = 10;
+  var worldYDimension = 10;
+
+  console.log(ViaGogoWorld.generateWorld(numEvents, worldXDimension, worldYDimension));
+
   // listener to read input and send to server to be processed
-  $('[name=find_tickets]').on('click', function(e) {
+  $(buttonSelector).on('click', function(e) {
     e.preventDefault();
 
-    console.log($('[name=coordinates]').val());
+    console.log($(inputSelector).val());
   });
 }());
