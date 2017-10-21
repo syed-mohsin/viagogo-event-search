93030a162bf17447f2765e1b8fa812999fcead7c
Syed Mohsin <syedm.90@gmail.com>
2017-10-20 20:02:43 -0400

    Initial commit: add main route and index files

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+// specify public assets directory
+app.use(express.static('public'));
+
+
+// index endpoint
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Server is listening at port', process.env.PORT || 3000);
+});
